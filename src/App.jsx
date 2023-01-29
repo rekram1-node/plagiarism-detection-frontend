49d762ecdd3c782d4bd8a85f4c5688b8812fff2a
davidherring123 <david.herring8680@gmail.com>
2023-01-29 12:52:38 -0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,56 +4,11 @@ import TextSubmission from './components/TextSubmission';
 
 function App() {
 
+    var myHeaders = new Headers();
+
     return (
         <div>
-            <nav class="navbar background" id="nav_background">
-                <ul class="nav-list">
-                    <div class="logo">
-                        <img src={"https://cdn-icons-png.flaticon.com/512/1905/1905047.png"} alt="logo"/>
-                    </div>
-                    <li><a href="https://github.com/rekram1-node/plagiarism-detection-backend">Documentation</a></li>
-                </ul>
-  
-                <div class="rightNav">
-                    
-                </div>
-            </nav>
-  
-            <section class="section">
-                <div class="box-main">
-                    <div class="firstHalf">
-                        <h1 class="text-big">
-                            Plagiarism Detector
-                        </h1>
-                        <p class="text-small">
-                          Paste Passages into the text boxes below.
-                        </p>
-                    </div>
-                </div>
-            </section>
-            <div class="text-area">
-                <TextSubmission />
-            </div>
-            <section class="section">
-                <div class="box-main">
-                    <div class="secondHalf">
-                    </div>
-                </div>
-            </section>
-
-            <section class="section">
-                <div class="box-main">
-                    <div class="secondHalf">
-                        
-                    </div>
-                </div>
-            </section>
-
-            <footer className="footer">
-                <p className="text-footer">
-                    Made by Aiden Cline, David Herring, Justin Herring, and Kieran Kahler
-                </p>
-            </footer>
+            <TextSubmission />
         </div>
     )
 }
--- src/components/TextSubmission.js
+++ src/components/TextSubmission.js
@@ -9,13 +9,28 @@ export default function TextArea() {
 
   const [response, setResponse] = useState({sentences:[], paragraphs:[], overallsimilarityScore:0})
 
-  const AreaStyle = {
-    label: {}, 
-    textarea: {
-      resize: "none",
-      padding: "30px"
-    }
+  myHeaders.append("Content-Type", "application/json");
+    
+  var text11 = "Normal science, the activity in which most scientists inevitably spend almost all their time, is predicated on the assumption that the scientific community knows what the world is like. Much of the success of the enterprise derives from the community's willingness to defend that assumption, if necessary at considerable cost. Normal science, for example, often suppresses fundamental novelties because they are necessarily subversive of its basic commitments (5).";
+  var text2 = "Normal science, the activity in which most scientists inevitably spend almost all their time, is predicated on the assumption that the scientific community knows what the world is like. Some scientists say that the success of the enterprise comes from the community’s willingness to defend that assumption, if necessary at considerable cost. Normal science often suppresses fundamental novelties because they are necessarily subversive of its basic commitments."
+  
+  var raw = JSON.stringify({
+    "documents": [
+      text1, text2
+    ]
+  });
+  
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
   };
+  
+  fetch("http://localhost:3000/plagiarism/compare", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
 
   const handleChange1 = (event) => setText1(event.target.value)
   const handleChange2 = (event) => setText2(event.target.value)
@@ -29,6 +44,8 @@ export default function TextArea() {
           console.log(err);
       })
   }
+
+  
   
   return (
     <form>
